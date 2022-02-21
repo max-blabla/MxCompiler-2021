3344acbb52032a7f3a7df08bd2b6794c22c5b70f
max-blabla <74357554+max-blabla@users.noreply.github.com>
2022-02-21 18:41:00 +0800

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 
 
+import ASTNode.ErrorInfo;
 import ASTNode.GlobalAST;
+import ASTNode.SemanticChecker;
 import CodeGenerator.CodeGenerator;
 import IRBuilder.IRBuilder;
 import MxParser.MxParser;
@@ -24,57 +26,53 @@ import javax.lang.model.type.TypeVisitor;
 
 public class Main {
     public static void main(String args[]) {
-        try {
-      //      String InputFile = "./Compiler-2021-testcases/codegen/e10.mx";
-          //  String IROutputFile = "./src/IROutput/test.ll";
-        //    String CGOutputFile = "D://Coding/ravel-master/build/test.s";
-            String CGOutputFile = "./src/output.s";
-            InputStream is = System.in;
-       //     InputStream is = new FileInputStream(InputFile);
+            try {
+                String InputFile;
+             //   InputFile = "./Compiler-2021-testcases/codegen/e" + 1 + ".mx";
+          //          String IROutputFile = "./src/IROutput/test.ll";
+    //                String CGOutputFile = "./src/CGOutput/test.s";
+                    //  String CGOutputFile = "D://Coding/ravel-master/build/test.s";
+                        String CGOutputFile = "./src/output.s";
+                       InputStream is = System.in;
+            //        InputStream is = new FileInputStream(InputFile);
 
-            ANTLRInputStream input = new ANTLRInputStream(is);
-            MxLexer lexer = new MxLexer(input);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            MxParser parser = new MxParser(tokens);
-            ParseTree tree = parser.program();
+                    ANTLRInputStream input = new ANTLRInputStream(is);
+                    MxLexer lexer = new MxLexer(input);
+                    CommonTokenStream tokens = new CommonTokenStream(lexer);
+                    MxParser parser = new MxParser(tokens);
+                    ParseTree tree = parser.program();
 
-            if (parser.getNumberOfSyntaxErrors() != 0) throw new RuntimeException();
-            ParseTreeWalker walker = new ParseTreeWalker();
-            ASTBuilder AstBuilder = new ASTBuilder();
-            walker.walk(AstBuilder, tree);
-            PostASTBuilder PostAstBuilder = new PostASTBuilder();
-            PostAstBuilder.SetProgram(AstBuilder.getGlobalAST());
-            PostAstBuilder.ParRevise();
-            PostAstBuilder.ConstSpread();
-            GlobalAST Program = PostAstBuilder.GetProgram();
-            IRBuilder IR = new IRBuilder(Program);
-     //       IR.setProgram(AST);
-            IR.ProgramIR();
-            PostIRBuilder PostIrBuilder = new PostIRBuilder();
-            PostIrBuilder.setModuleList(IR.GetModuleList());
-         //   PostIrBuilder.BlockMerging();
-            PostIrBuilder.RemoveRedundant();
+                    if (parser.getNumberOfSyntaxErrors() != 0) throw new RuntimeException();
+                    ParseTreeWalker walker = new ParseTreeWalker();
+                    ASTBuilder AstBuilder = new ASTBuilder();
+                    walker.walk(AstBuilder, tree);
+                    SemanticChecker SC = new SemanticChecker();
+                    SC.SetProgram(AstBuilder.getGlobalAST());
+                    SC.SemanticCheck();
+                    PostASTBuilder PostAstBuilder = new PostASTBuilder();
+                    PostAstBuilder.SetProgram(AstBuilder.getGlobalAST());
+                    PostAstBuilder.ConstSpread();
+                    GlobalAST Program = PostAstBuilder.GetProgram();
+                    IRBuilder IR = new IRBuilder(Program);
+                    IR.ProgramIR();
+                    PostIRBuilder PostIrBuilder = new PostIRBuilder();
+                    PostIrBuilder.setModuleList(IR.GetModuleList());
+                    //   PostIrBuilder.BlockMerging();
+                    PostIrBuilder.RemoveRedundant();
 
-          //  IROutput IROut = new IROutput(IR.GetModuleList());
-        //    IROut.FileRun(IROutputFile);
-            CodeGenerator CodeGen = new CodeGenerator();
-            CodeGen.setModuleList(IR.GetModuleList());
-            CodeGen.CodeGenerate();
-            PrintStream Os = new PrintStream(CGOutputFile);
-            CodeGen.CodeOutput(Os);
-            /*TypeVisitor typeVisitor = new TypeVisitor(parser);
-            walker.walk(typeVisitor, tree);
-
-            GlobalVisitor globalVisitor = new G
-
-            lobalVisitor(typeVisitor.GetTypeMap(), parser);
-            walker.walk(globalVisitor, tree);
-
-            SemanticChecker semanticChecker = new SemanticChecker(globalVisitor.GetGlobalTable(), parser);
-            walker.walk(semanticChecker, tree);*/
-        } catch (java.io.IOException E) {
-            System.out.print("Input Error");
-        }
-
+            //        IROutput IROut = new IROutput(IR.GetModuleList());
+            //        IROut.FileRun(IROutputFile);
+                    CodeGenerator CodeGen = new CodeGenerator();
+                    CodeGen.setModuleList(IR.GetModuleList());
+                    CodeGen.CodeGenerate();
+                    PrintStream Os = new PrintStream(CGOutputFile);
+                    CodeGen.CodeOutput(Os);
+                    is.close();
+       //         }
+            } catch (java.io.IOException E) {
+                System.out.print("Input Error");
+            }  catch (ErrorInfo E){
+                throw new RuntimeException();
+            }
     }
 }
